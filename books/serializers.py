c3de7ce9e6c4972de5994627c8e09a766bf95806
piotr bronikowski <pbrqsl@gmail.com>
2023-11-06 09:48:46 +0100

    remove null values from dict

--- books/serializers.py
+++ books/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 from books.models import Book
+from collections import OrderedDict
 
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret_no_null = OrderedDict()
+        for item in ret:
+            if ret[item]:
+                ret_no_null[item] = ret[item]
+        return ret_no_null
