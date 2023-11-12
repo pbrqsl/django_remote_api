de8f75dc3554b7b006a10754283a42f6527417d4
piotr bronikowski <pbrqsl@gmail.com>
2023-11-12 16:51:13 +0100

    remove old code which will not be used

--- db/urls.py
+++ db/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from db.views import pull_books, pull_books_with_serializer
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
     path("", pull_books_with_serializer, name="book-pull")
-    #path("", pull_books, name="book-pull")
 ]
--- db/views.py
+++ db/views.py
@@ -9,48 +9,6 @@ import json
 
 
 @api_view(["POST"])
-def pull_books(request):
-    URL = "https://www.googleapis.com/books/v1/volumes"
-
-    body = json.loads(request.body.decode("utf-8"))
-    params = dict(q=body["q"])
-    response = requests.get(url=URL, params=params)
-    books = response.json()
-    for book in books["items"]:
-        title = book["volumeInfo"]["title"]
-        authors = book["volumeInfo"].get("authors")
-        categories = book["volumeInfo"].get("categories")
-        published_date = book["volumeInfo"].get("publishedDate")
-        # thumbnail = book['volumeInfo']['imageLinks'].get('thumbnail')
-        thumbnail = book["volumeInfo"].get("imageLinks", {}).get("thumbnail")
-        average_rating = book["volumeInfo"].get("averageRating")
-        ratings_count = book["volumeInfo"].get("ratingsCount")
-
-        #print(Book.objects.filter(title=title))
-
-        if not Book.objects.filter(title=title):
-            print("---------ADDING BOOK-------------")
-            new_book = Book(
-                title=title,
-                authors=authors,
-                published_date=published_date,
-                categories=categories,
-                thumbnail=thumbnail,
-                average_rating=average_rating,
-                ratings_count=ratings_count,
-            )
-            new_book.save()
-
-        # authors = book['volumeInfo']['authors']
-        # print(title)
-        # print(authors)
-        # new_book = Book(title = title)
-        # print(new_book.title)
-        # new_book.save()
-
-    return Response({"message": "books_indexed"})
-
-@api_view(["POST"])
 def pull_books_with_serializer(request):
     URL = "https://www.googleapis.com/books/v1/volumes"
 
@@ -64,8 +22,5 @@ def pull_books_with_serializer(request):
 
     serialized_books_in.save()
     
-    
-
     return Response({"message": "books_indexed"})
 
-# Create your views here.
